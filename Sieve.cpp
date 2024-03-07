f1b19fabee12df9487e083299e6dbf58d512f736
Pradeep <98871228+Inferno-coder@users.noreply.github.com>
2024-03-07 12:33:38 +0530

    Create Sieve.cpp

---
+++ Sieve.cpp
@@ -0,0 +1,20 @@
+class Sieve{
+    public:
+    bool *isPrime;
+    Sieve(int size){
+    isPrime = new bool[size];
+        for (int i = 0; i <= size; ++i) {
+            isPrime[i] = true;
+        }
+    isPrime[0]=false;
+    isPrime[1]=false;
+    createSieve(size);
+    }
+    void createSieve(int n){
+        for(int i=2;i*i<=n;i++){
+            if(isPrime[i]==true){
+                for(int j=i*i;j<=n;j+=i)isPrime[j]=false;
+            }
+        }
+    } 
+};
