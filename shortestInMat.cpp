2f5f3da12b9ff6cdf11705be305d4209901f8399
Pradeep <98871228+Inferno-coder@users.noreply.github.com>
2024-03-27 23:55:43 +0530

    Update shortestInMat.cpp

--- shortestInMat.cpp
+++ shortestInMat.cpp
@@ -1,91 +1,47 @@
-class Node
-{
-    public:
-    int x;
-    int y;
-    int cnt;
-};
-class Solution
-{
-    int dr[4] = {0,0,-1,1};
-    int dc[4] = {-1,1,0,0};
-    
-    bool isSafe(int newX, int newY, int n, int m)
-    {
-        if(newX >=0 && newY >=0 && newX < n && newY < m) return true;
-        
-        return false;
-    }
-    
+class Solution {
 public:
+    int dir[5] = {0, -1, 0, 1, 0};
 
-    int findShortestPath(vector<vector<int>> &mat)
-    {
-        int n = mat.size();
-        int m = mat[0].size();
-        
-        for(int i = 0; i < n; i++)
-        {
-            for(int j = 0; j < m; j++)
-            {
-                if(mat[i][j] == 0)
-                {
-                    for(int k = 0; k < 4; k++)
-                    {
-                        int newX = i + dr[k];
-                        int newY = j + dc[k];
-                        
-                        if(isSafe(newX, newY, n, m))
-                        {
-                            mat[newX][newY] = -1;
-                        }
-                    }
-                }
+    void dfs(int row, int col, vector<vector<int>>& mat, vector<vector<int>>& vis, int totRow, int totCol, int& mini, int sum) {
+        if (row < 0 || row >= totRow || col < 0 || col >= totCol || mat[row][col] == 0 || vis[row][col])
+            return;
+
+        if (col == totCol - 1) {
+            if(mat[row][col]==1){
+            mini = min(mini, sum);
+            }
+            return;
+        }
+
+        vis[row][col] = 1;
+
+        for (int i = 0; i < 4; i++) {
+            int newr = row + dir[i];
+            int newc = col + dir[i + 1];
+
+            if (newr >= 0 && newr < totRow && newc >= 0 && newc < totCol && mat[newr][newc] == 0)
+                return;
+        }
+
+        dfs(row - 1, col, mat, vis, totRow, totCol, mini, sum + 1);
+        dfs(row + 1, col, mat, vis, totRow, totCol, mini, sum + 1);
+        dfs(row, col - 1, mat, vis, totRow, totCol, mini, sum + 1);
+        dfs(row, col + 1, mat, vis, totRow, totCol, mini, sum + 1);
+
+        vis[row][col] = 0;
+    }
+
+    int findShortestPath(vector<vector<int>>& mat) {
+        int row = mat.size();
+        int col = mat[0].size();
+        int mini = INT_MAX;
+        vector<vector<int>> vis(row, vector<int>(col, 0));
+
+        for (int i = 0; i < row; i++) {
+            if (mat[i][0] == 1) {
+                dfs(i, 0, mat, vis, row, col, mini, 1);
             }
         }
-        
-        int ans = -1;
-        
-        queue<Node> q;
-        vector<vector<bool>> vis(n, vector<bool>(m,false));
-        
-        for(int i = 0; i < n; i++)
-        {
-            if(mat[i][0] == 1)
-            {
-                q.push({i,0,1});
-            }
-        }
-        
-        ans = INT_MAX;
-        while(!q.empty())
-        {
-            int x = q.front().x;
-            int y = q.front().y;
-            int cnt= q.front().cnt;
-            
-            q.pop();
-            if(y == m - 1)
-            {
-                return cnt;
-            }
-            
-            vis[x][y] = true;
-            
-            for(int k = 0; k < 4; k++)
-            {
-                int newX = x + dr[k];
-                int newY = y + dc[k];
-                
-                if(isSafe(newX, newY, n, m) && vis[newX][newY] == false && mat[newX][newY] == 1)
-                {
-                    q.push({newX, newY, cnt + 1});
-                }
-            }
-        }
-        
-        return -1;
-        
-        
+        return mini == INT_MAX ? -1 : mini;
     }
 };
