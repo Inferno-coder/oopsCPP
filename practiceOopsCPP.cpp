43c19f2cfb0b0a14222f2cdb78397e23e715fbc2
Pradeep <98871228+Inferno-coder@users.noreply.github.com>
2024-04-20 08:09:58 +0530

    Update practiceOopsCPP.cpp

--- practiceOopsCPP.cpp
+++ practiceOopsCPP.cpp
@@ -1,8 +1,20 @@
-#include <stdio.h>
-
+#include <iostream>
+using namespace std;
+template<class t>
+void swapping(t &a,t &b){
+    t temp=a;
+    a=b;
+    b=temp;
+}
 int main()
 {
-    printf("Hello World");
-
+    char ch1='a';
+    char ch2='b';
+    int a =1;
+    int b=2;
+    swapping(ch1,ch2);
+    swapping(a,b);
+    cout<<ch1<<" "<<ch2<<endl;
+    cout<<a<<" "<<b<<endl;
     return 0;
 }
