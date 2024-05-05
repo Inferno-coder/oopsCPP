d852d46ee8d3351953d0d063b82584f9fae77ec0
Pradeep <98871228+Inferno-coder@users.noreply.github.com>
2024-05-05 22:15:54 +0530

    Create deleteNode.cpp

---
+++ deleteNode.cpp
@@ -0,0 +1,8 @@
+void deleteNode(ListNode* node) {
+        if(node->next==NULL)node=NULL;
+        else
+        {
+            node->val=node->next->val;
+            node->next=node->next->next;
+        }
+}
